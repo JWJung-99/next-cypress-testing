4495ccf7b313abd38306fb041f745198da46fc9e
JWJung-99 <39busy@naver.com>
2025-06-03 19:36:22 +0900

    Chapter06: 두 번째 테스트 코드 작성

---
+++ cypress/e2e/product-page.cy.js
@@ -0,0 +1,22 @@
+describe('상품 목록 페이지', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+
+	// 첫 번째 테스트 시나리오
+	it('페이지에 진입했을 때 상품 목록이 표시된다.', () => {
+		cy.getByCy('product-item').should('be.visible');
+	});
+
+	// 두 번째 테스트 시나리오
+	it('내비게이션 바의 장바구니 링크를 클릭하면 장바구니 페이지로 이동한다.', () => {
+		// prepare (준비)
+		// cy.visit();
+
+		// action (사용자 인터렉션)
+		cy.getByCy('cart-link').click();
+
+		// assertion (보장)
+		cy.url().should('include', '/cart');
+	});
+});
--- layouts/BasicLayout.jsx
+++ layouts/BasicLayout.jsx
@@ -5,7 +5,10 @@ function BasicLayout({ children }) {
 	return (
 		<main>
 			<nav>
-				<Link href="/">Product</Link> | <Link href="/cart">Cart</Link>
+				<Link href="/">Product</Link> |{' '}
+				<Link data-cy="cart-link" href="/cart">
+					Cart
+				</Link>
 			</nav>
 			<div>{children}</div>
 		</main>
